3ac42c6c8f49571148577460bf4f8637157cb42b
glaz <glaz@astra>
2023-04-25 19:55:04 +0300

    payload_1

--- parser/webhooks_api/parsing.py
+++ parser/webhooks_api/parsing.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 import requests
-from pprint import pprint
 
 load_dotenv()
 
--- parser/webhooks_api/views.py
+++ parser/webhooks_api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from pprint import pprint
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,5 +8,5 @@ from .parsing import parse_last_post
 @api_view(['POST'])
 def add_new_post(request):
     post = parse_last_post()
-    print(request.data)
+    pprint(request.data)
     return Response(post)
