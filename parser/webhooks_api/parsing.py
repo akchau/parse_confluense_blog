9188edd8e96a60cb9f8514dbcc525462185bc589
glaz <glaz@astra>
2023-04-25 23:25:35 +0300

    Подключение к ftp сервру_4

--- parser/webhooks_api/parsing.py
+++ parser/webhooks_api/parsing.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 from ftplib import FTP
+from html.parser import HTMLParser
 
 load_dotenv()
 
@@ -10,32 +11,46 @@ password = os.getenv('PASSWORD')
 
 auth = requests.auth.HTTPBasicAuth(username, password)
 headers = {
-   "Accept": "application/json",
-   "body-format": "atlas_doc_format"
+    "Accept": "application/json",
+    "body-format": "atlas_doc_format"
 }
 
-def get_files(links):
+
+class BodyParser(HTMLParser):
+    links = []
+    def handle_data(self, data):
+        self.links.append(data)
+        return self.links
+
+
+def get_files(link):
     domain = 'server-dev.astralinux.ru'
+    ftp_dir = link.split(f'ftp://{domain}')[1][1:]
+    print(ftp_dir)
     ftp = FTP(domain)
     ftp.login()
-    ftp.cwd('maintainers/frozen/alse-X.7/1.7/RC/1.7.4.7') 
+    ftp.cwd(ftp_dir) 
     ftp.retrlines('LIST')
 
 
 def parse_last_post(id):
-   url = f'http://glazarev.atlassian.net/wiki/api/v2/blogposts/{id}?body-format=storage'
-   response = requests.request(
-      "GET",
-      url,
-      headers=headers,
-      auth=auth,
-   )
-   data = response.json().get('body').get('storage').get('value') # .get('title')
-   return data
+    url = f'http://glazarev.atlassian.net/wiki/api/v2/blogposts/{id}?body-format=storage'
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth,
+    )
+    data = response.json().get('body').get('storage').get('value') # .get('title')
+    parser = BodyParser()
+    parser.feed(data)
+    print(parser.links[3])
+    return get_files(parser.links[3])
 
 
 def main():
-    print(get_files())
+    # print(get_files())
+    print(parse_last_post(786563))
 
 
 if __name__ == "__main__":
